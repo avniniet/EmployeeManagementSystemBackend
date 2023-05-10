5f9f3fbf0d421e0f93a1578fe1ba9598db648519
Avnish <avniniet@gmail.com>
2023-05-10 21:00:13 +0530

    CRUD complete: 10 May2023

--- EmployeeManagementSystemBackend/src/main/java/in/projectdevelopment/ems/controller/EmployeeController.java
+++ EmployeeManagementSystemBackend/src/main/java/in/projectdevelopment/ems/controller/EmployeeController.java
@@ -1,15 +1,25 @@
 package in.projectdevelopment.ems.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.projectdevelopment.ems.model.Employee;
 import in.projectdevelopment.ems.service.EmployeeService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
@@ -21,5 +31,35 @@ public class EmployeeController {
 	public List<Employee> getAllEmployee(){
 		System.out.println("Get all employee controller");
 		return employeeService.getAllEmployee();
+	}
+	 
+	//Create employee rest API
+	@PostMapping("/employee")
+	public Employee createEmployee(@RequestBody Employee employee) {	
+		
+		return employeeService.createEmployee(employee);
+	}
+	
+	//get employee by id restAPI
+	@GetMapping("/employee/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+			Employee employee =employeeService.getEmployeeById(id);
+		return ResponseEntity.ok(employee);
+		
+	}
+	
+	@PutMapping("/employee/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee updatedEmployee = employeeService.updateEmployee(id, employeeDetails);
+		return ResponseEntity.ok(updatedEmployee);
+		
+	}
+	
+	@DeleteMapping("/employee/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		this.employeeService.deleteEmployee(id);
+		Map<String,Boolean> response = new HashMap<>();
+		response.put("Employee deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 }
--- EmployeeManagementSystemBackend/src/main/java/in/projectdevelopment/ems/service/EmployeeService.java
+++ EmployeeManagementSystemBackend/src/main/java/in/projectdevelopment/ems/service/EmployeeService.java
@@ -3,8 +3,11 @@ package in.projectdevelopment.ems.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import in.projectdevelopment.ems.exception.ResourceNotFoundException;
 import in.projectdevelopment.ems.model.Employee;
 import in.projectdevelopment.ems.repository.EmployeeRepository;
 
@@ -18,5 +21,33 @@ public class EmployeeService {
 	  
 	  return employeeRepository.findAll();
   }
+  
+  public Employee createEmployee(Employee employee) {
+	  return employeeRepository.save(employee);
+  }
 
+  public Employee getEmployeeById(Long id) {
+	  return employeeRepository.
+			  findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee is not present with employee id "+id));
+  }
+  
+  public Employee updateEmployee(Long id, Employee employeeDetails) {
+	  Employee employee = employeeRepository.
+	  findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee is not present with employee id "+id));
+	  
+	  employee.setFirstName(employeeDetails.getFirstName());
+	  employee.setLastName(employeeDetails.getLastName());
+	  employee.setEmailId(employeeDetails.getEmailId());
+	  
+	  Employee updatedEmployee = employeeRepository.save(employee);
+	  return updatedEmployee;
+  }
+  
+  public void deleteEmployee(Long id) {
+	  Employee employee = employeeRepository.
+			  findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee is not present with employee id "+id));
+	  employeeRepository.delete(employee);
+	  
+	  
+  }
 }
